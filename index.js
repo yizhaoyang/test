fa95fb94e57377e365ab4f55c1e8fad6749c30e4
yizhaoyang <yizhaoyang1224@gmail.com>
2018-05-31 09:03:16 +0200

    show and hide

--- index.js
+++ index.js
@@ -12,3 +12,30 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+#normal {
+  display: inline
+  position: relative
+}
+
+#hiden {
+  display: none
+  position: relative
+}
+
+
+var content = document.getElementById("conetnt");
+var button = document.getElementById("show-more");
+
+button.onclick = function(){
+  if(content.className=="open"){
+    //shrink the box
+    content.className="";
+    button.innerHTML="show more"
+  } else{
+    //expand the box
+    content.className="open";
+    button.innerHTML="show less"
+  }
+}
